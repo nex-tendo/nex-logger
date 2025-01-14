68c5e281153e9ec584dce3a0186df773a7c797d3
cedkeChat <123301168+00cedke@users.noreply.github.com>
2025-01-14 19:31:26 +0100

    Update logger.go

--- logger.go
+++ logger.go
@@ -4,40 +4,53 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
-func getCurrentTime() string {
-	return time.Now().Format("2006-01-02 15:04:05")
+type Logger struct {
+	file   *os.File
+	logger *log.Logger
+	mu     sync.Mutex
 }
 
-func Info(message string) {
-	logMessage("INFO", message)
-}
-
-func Error(message string) {
-	logMessage("ERROR", message)
-}
-
-func Warning(message string) {
-	logMessage("WARNING", message)
-}
-
-func Success(message string) {
-	logMessage("SUCCESS", message)
-}
-
-func logMessage(level, message string) {
-	fmt.Printf("[%s] %s: %s\n", getCurrentTime(), level, message)
-}
-
-func LogToFile(level, message string) {
-	file, err := os.OpenFile("logs.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+func NewLogger(filePath string) (*Logger, error) {
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Fatalf("Error opening log file: %v", err)
+		return nil, fmt.Errorf("error opening log file: %v", err)
 	}
-	defer file.Close()
 
-	logger := log.New(file, "", 0)
-	logger.Printf("[%s] %s: %s\n", getCurrentTime(), level, message)
+	return &Logger{
+		file:   file,
+		logger: log.New(file, "", 0),
+	}, nil
+}
+
+func (l *Logger) Close() {
+	l.file.Close()
+}
+
+func (l *Logger) logMessage(level, message string) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	logMsg := fmt.Sprintf("[%s] %s: %s", timestamp, level, message)
+	fmt.Println(logMsg)
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.logger.Println(logMsg)
+}
+
+func (l *Logger) Info(message string) {
+	l.logMessage("INFO", message)
+}
+
+func (l *Logger) Error(message string) {
+	l.logMessage("ERROR", message)
+}
+
+func (l *Logger) Warning(message string) {
+	l.logMessage("WARNING", message)
+}
+
+func (l *Logger) Success(message string) {
+	l.logMessage("SUCCESS", message)
 }
